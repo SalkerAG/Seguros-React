e2b8372817aa99ae8705503ef19d75260d3269b8
Juan Enrique Aicardo García <salkerps3@gmail.com>
2021-02-07 13:35:43 +0100

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './components/Header'
+import Resumen from './components/Resumen'
+import Resultado from './components/Resultado'
+import Formulario from './components/Formulario'
+import Spinner from './components/Spinner'
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
+
 
 function App() {
+
+  const [resumen, setResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: ''
+    }
+  });
+
+  const [cargando, setCargando] = useState(false);
+
+  const {cotizacion, datos} = resumen
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Contenedor>
+        <Header 
+          titulo = "Cotizador de Seguros"
+        />
+        <ContenedorFormulario>
+          <Formulario 
+            setResumen={setResumen}
+            setCargando={setCargando}
+          />
+          {cargando ? <Spinner /> : null}
+          
+          
+          {!cargando ? 
+            <>
+              <Resumen 
+                datos={datos}
+              />
+              <Resultado 
+              cotizacion={cotizacion}
+              />
+            </>
+            : null
+          }
+          
+        </ContenedorFormulario>
+      </Contenedor>
+    </>
   );
 }
 
